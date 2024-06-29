24ab9a3c492d605137206952c047714d4403ac0d
Mateja Nikolić <kima.x159@gmail.com>
2024-06-29 22:53:43 +0200

    Added actual functionality and made it somewhat idiotproof

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+import random
 
 #Create window and give it a title
 window = tk.Tk()
 window.title("Secure Password Generator")
 
 #Some static variables
-windowWidth = 500
-windowHeight = 500
+windowWidth = 420
+windowHeight = 164
 screenWidth = window.winfo_screenwidth()
 screenHeight = window.winfo_screenheight()
 xCenter = int((screenWidth - windowWidth) / 2)
@@ -22,23 +24,25 @@ window.resizable(False, False)
 #Intro text
 label = ttk.Label(
     window,
-    text = "Select your settings"
+    text = "Select your settings",
+    font = 24
 )
 label.grid(
     column = 0,
-    columnspan = 3,
-    row = 0
+    columnspan = 4,
+    row = 0,
+
 )
 
 #Password length slider
 scaleValue = tk.IntVar()
-def getValue():
+def getPassLength():
     return int(passLengthScale.get())
 def passLengthChanged(event):
-    passLengthLabel.configure(text = "Password length: {:>2}".format(getValue()))
+    passLengthLabel.configure(text = "Password length: {:>2}".format(getPassLength()))
 passLengthLabel = ttk.Label(
     window,
-    text = "Password length: 1",
+    text = "Password length: ",
 )
 passLengthLabel.grid(
     column = 0,
@@ -53,6 +57,7 @@ passLengthScale = ttk.Scale(
     command = passLengthChanged,
     variable = scaleValue
 )
+passLengthScale.set(12)
 passLengthScale.grid(
     column = 1,
     columnspan = 2,
@@ -72,7 +77,7 @@ charSelectLabel.grid(
     padx = 4,
     sticky = 'w',
 )
-includeLower = tk.IntVar()
+includeLower = tk.IntVar(value = 1)
 includeLowerCheck = ttk.Checkbutton(
     window,
     text = 'Lowercase(a-z)',
@@ -85,7 +90,7 @@ includeLowerCheck.grid(
     sticky = 'w',
     padx = 4
 )
-includeUpper = tk.IntVar()
+includeUpper = tk.IntVar(value = 1)
 includeUpperCheck = ttk.Checkbutton(
     window,
     text = 'Uppercase(A-Z)',
@@ -98,7 +103,7 @@ includeUpperCheck.grid(
     sticky = 'w',
     padx = 4
 )
-includeNumbers = tk.IntVar()
+includeNumbers = tk.IntVar(value = 1)
 includeNumbersCheck = ttk.Checkbutton(
     window,
     text = 'Numbers(0-9)',
@@ -111,7 +116,7 @@ includeNumbersCheck.grid(
     sticky = 'w',
     padx = 4
 )
-includeSpecial = tk.IntVar()
+includeSpecial = tk.IntVar(value = 1)
 includeSpecialCheck = ttk.Checkbutton(
     window,
     text = 'Special(!,@,#,...)',
@@ -148,6 +153,61 @@ initStringEntry.grid(
     row = 5,
     padx = 4,
     sticky = 'w'
+)
+
+#Password listbox
+passListBox = tk.Listbox(
+    window,
+    height = 1,
+    width = 51,
+    justify = 'center'
+)
+passListBox.grid(
+    column = 0,
+    columnspan = 5,
+    row = 7,
+    padx = 4
+)
+
+#Generate passwords button
+lowercaseLettersList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+uppercaseLettersList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+specialCharactersList = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '', '{', '|', '}', '~']
+def generatePass():
+    if len(initString.get()) > getPassLength():
+        messagebox.showerror("Error", "Initial text is longer than password length.")
+        return
+    finalCharactersList = []
+    if includeLower.get() == 1:
+        finalCharactersList.extend(lowercaseLettersList)
+    if includeUpper.get() == 1:
+        finalCharactersList.extend(uppercaseLettersList)
+    if includeNumbers.get() == 1:
+        finalCharactersList.extend(numbersList)
+    if includeSpecial.get() == 1:
+        finalCharactersList.extend(specialCharactersList)
+    if len(finalCharactersList) == 0:
+        messagebox.showerror("Error", "You have to select atleast one group of characters.")
+        return
+    password = initString.get()
+    for i in range(getPassLength() - len(initString.get())):
+        randomChar = random.choice(finalCharactersList)
+        password += randomChar
+    window.clipboard_clear()
+    window.clipboard_append(password)
+    passListBox.insert(0, password)
+generatePassButton = ttk.Button(
+    window,
+    text = 'Generate password',
+    command = generatePass
+)
+generatePassButton.grid(
+    column = 0,
+    columnspan = 5,
+    row = 6,
+    ipadx = 100,
+    pady = 4
 )
 
 window.mainloop()
